629442ff06a1f53b2da2c5a2daadf979561fdf04
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-08 03:48:12 +0500

    working

--- src/app/layouts/partials/header/header.component.ts
+++ src/app/layouts/partials/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  public notificaitions
+
+  constructor() {
+    this.notificaitions = [
+      { id: 1, message: 'New comment on your post', read: false },
+      { id: 2, message: 'Your profile was viewed', read: false },
+      { id: 3, message: 'New follower', read: true }
+    ];
+  }
+
+  fetchNotifications() {
+    // Simulate fetching notifications from a service
+    return this.notificaitions;
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
