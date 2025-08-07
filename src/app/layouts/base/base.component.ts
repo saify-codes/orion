c9966bc91b90e330e464bc7c4dc580fc30cc9dcb
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-08 02:47:29 +0500

    working

--- src/app/layouts/base/base.component.ts
+++ src/app/layouts/base/base.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../partials/header/header.component";
+import { SidebarComponent } from "../partials/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-base',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent],
   templateUrl: './base.component.html',
   styleUrl: './base.component.css'
 })
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../layouts/partials/header/header.component";
 import { SidebarComponent } from "../../layouts/partials/sidebar/sidebar.component";
+import { BaseComponent } from "../../layouts/base/base.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, BaseComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  items = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`);
 }
