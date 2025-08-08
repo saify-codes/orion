5931b58f64a31873d71d0db6bf787f5b1c21112c
saify <sarfaraz.symits@gmail.com>
2025-08-08 15:03:19 +0500

    working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GeneralComponent } from './pages/settings/general/general.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    {
+        path: 'settings',
+        children: [
+            { path: 'general', component: GeneralComponent}
+        ]
+    },
     { path: 'signin', component: SigninComponent },
 ];
---
+++ src/app/pages/settings/general/general.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BaseComponent } from "../../../layouts/base/base.component";
+
+@Component({
+  selector: 'app-general',
+  imports: [BaseComponent],
+  templateUrl: './general.component.html',
+  styleUrl: './general.component.css'
+})
+export class GeneralComponent {
+
+}
