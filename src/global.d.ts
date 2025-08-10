ec69e1fd23d01857c301b7808dfd2b5418f0f5d0
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-10 07:00:43 +0500

    working

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,11 @@
-import type { IStaticMethods } from 'preline/dist';
+import type { IStaticMethods } from "preline/dist";
 
 declare global {
   interface Window {
     // Optional third-party libraries
     _;
-    $: typeof import('jquery');
-    jQuery: typeof import('jquery');
+    $: typeof import("jquery");
+    jQuery: typeof import("jquery");
     DataTable;
     Dropzone;
     VanillaCalendarPro;
