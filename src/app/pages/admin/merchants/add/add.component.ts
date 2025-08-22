cbcdf2f4f568cf899686dae3439df9ff28fe21f1
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-23 03:45:31 +0500

    working

--- src/app/pages/admin/merchants/add/add.component.ts
+++ src/app/pages/admin/merchants/add/add.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css'
 })
 export class AddMerchantComponent {
+
+  public  form:FormGroup  = inject(FormBuilder).group({
+    email:    ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
 
 }
