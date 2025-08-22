d8243f6ad88b26ecee49c332542cbc52fc7690c6
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-23 03:09:37 +0500

    working

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  private location = inject(Location);
+  private router   = inject(Router);
 
+  goBack() {
+    // If there’s history, go back; otherwise, send to a safe fallback (e.g., home)
+    if (history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigateByUrl('/'); // fallback route
+    }
+  }
 }
