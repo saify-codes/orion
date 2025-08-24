3ba0f90198abfb84e93e57d1b30c4a2af3e47621
saify <dev.muhammadsarfaraz@gmail.com>
2025-08-25 03:30:29 +0500

    working

--- src/app/pages/admin/admins/add/add.component.ts
+++ src/app/pages/admin/admins/add/add.component.ts
@@ -8,6 +8,16 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './add.component.css'
 })
 export class AddAdminComponent {
+  permissions = [
+    'users.read','users.create','users.update','users.delete',
+    'roles.read','roles.create','roles.update','roles.delete',
+    'posts.read','posts.create','posts.update','posts.delete',
+    'comments.read','comments.create','comments.update','comments.delete',
+    'settings.read','settings.update',
+    'billing.read','billing.update',
+    'analytics.view','support.respond','notifications.send','backups.run','system.reboot',
+  ];
+  
   private fb = inject(FormBuilder)
   public form:FormGroup = this.fb.group({
 
