b086ca6b0143b489240da081840cbc5457618a94
saify <dev.muhammadsarfaraz@gmail.com>
2025-09-29 01:30:26 +0500

    auth

--- src/app/interceptors/base-url.interceptor.ts
+++ src/app/interceptors/base-url.interceptor.ts
@@ -1,10 +1,10 @@
 // src/app/interceptors/base-url.interceptor.ts
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
-import { environment } from '../../environments/local';
+import { environment } from '../../environments/environment';
 
 export const BaseUrlInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
   const apiReq = req.clone({
-    url: `${environment.apiBaseUrl}/${req.url}`,
+    url: `${environment.baseURL}/${req.url}`,
   });
   return next(apiReq);
 };
