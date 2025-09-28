8b0f1e444f223a7a711836e3bf90f96077941643
saify <dev.muhammadsarfaraz@gmail.com>
2025-09-29 01:28:10 +0500

    auth

--- src/app/services/merchant/auth.service.ts
+++ src/app/services/merchant/auth.service.ts
@@ -2,27 +2,26 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Cookie } from '../../utils/cookie';
+import { environment } from '../../../environments/environment';
 
 export type User = {
   name: string;
   email: string;
   avatar: string | null;
   type: 'ADMIN' | 'STAFF';
-  additionalData: any
+  additionalData: any;
 };
 
 // Storage keys
 const COOKIE_KEY = 'merchant_auth_session';
 
 @Injectable({ providedIn: 'root' })
-
 export class MerchantAuthService {
-    
-  public user: User | null    = null;
+  public user: User | null = null;
   public token: string | null = null;
-  public status: string       = 'loading';
-  private http: HttpClient    = inject(HttpClient);
-  private router: Router      = inject(Router);
+  public status: string = 'loading';
+  private http: HttpClient = inject(HttpClient);
+  private router: Router = inject(Router);
 
   // ----------- Lifecycle -----------
 
@@ -30,12 +29,15 @@ export class MerchantAuthService {
    * Initialize auth state before the app renders.
    * Use APP_INITIALIZER to call this and block bootstrap until it resolves.
    */
-  init(){
+  init() {
     const cookie = Cookie.get(COOKIE_KEY) ?? {}; // {} if null
-    const { user = null, token = null } = cookie as {user?: any, token?: string};
+    const { user = null, token = null } = cookie as {
+      user?: any;
+      token?: string;
+    };
 
-    this.user   = user;
-    this.token  = token;
+    this.user = user;
+    this.token = token;
     this.status = user ? 'authenticated' : 'unauthenticated';
   }
 
@@ -50,27 +52,25 @@ export class MerchantAuthService {
     password: string,
     remember: boolean
   ): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.http
-        .post('https://orion-backend-main-l6ntcw.laravel.cloud/api/admin/auth/login', { username, password, remember })
-        .subscribe({
-          next: (res: any) => {
-            const { user, token, maxAge } = res.data;
-            this.user = user;
-            this.token = token;
-            this.status = 'authenticated';
+    try {
+      const response = await fetch(`${environment.baseURL}/login`);
+      const data = await response.json();
 
-            Cookie.set(COOKIE_KEY, { user, token }, { maxAge });
+      if (!response.ok) {
+        throw new Error(data.message || 'something went wrong');
+      }
 
-            this.router.navigate(['/']);
+      const { data: user, token, maxAge } = data;
+      this.user = user;
+      this.token = token;
+      this.status = 'authenticated';
 
-            resolve();
-          },
-          error: ({ error }) => {
-            reject(error);
-          },
-        });
-    });
+      Cookie.set(COOKIE_KEY, { user, token }, { maxAge });
+
+      this.router.navigate(['/']);
+    } catch (error) {
+      throw new Error('Json parse error');
+    }
   }
 
   async logout(): Promise<void> {
@@ -79,11 +79,10 @@ export class MerchantAuthService {
   }
 
   // ----------- Helpers -----------
-
   private clearAuthSession() {
-    this.user    = null;
-    this.token   = null;
-    this.status  = 'unauthenticated';
+    this.user = null;
+    this.token = null;
+    this.status = 'unauthenticated';
     Cookie.delete(COOKIE_KEY);
   }
 }
--- src/environments/local.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  apiBaseUrl: 'https://orion-main-edyifn.laravel.cloud/api',
-};
